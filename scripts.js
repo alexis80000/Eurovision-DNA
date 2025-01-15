03489f408a460397019bfaf0241041b31b4a1751
alexis80000 <alexisvfernandes007@gmail.com>
2025-01-15 11:17:05 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,3 +3,9 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log("Eurovision DNA is live!");
 });
 
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+toggleSwitch.addEventListener('change', () => {
+    document.body.classList.toggle('dark-mode');
+});
+
+
